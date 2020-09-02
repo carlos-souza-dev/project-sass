4b7691fdeff2f0cd4de7403d0ccfefdc7ddc9589
carlos-souza-02 <carlos.alberto.g.s@icloud.com>
2020-09-02 11:59:02 -0300

    Version 1.0.0

---
+++ js/couserl.js
@@ -0,0 +1,40 @@
+(function(){
+var figure = document.querySelectorAll(".coursel figure");
+var prev = document.querySelector(".coursel .prev")
+var next = document.querySelector(".coursel .next")
+
+    figure[0].classList.add('show');
+    var counter = 0;
+    
+    
+    prev.addEventListener('click', function(){
+        
+            if(counter == 0){
+                counter = 3;
+                figure[counter-3].classList.remove('show');
+                figure[counter].classList.add('show');
+            } else if (counter > 0){
+                figure[counter].classList.remove('show');
+                counter--;
+                figure[counter].classList.add('show');
+            }
+        
+        console.log("prev",counter);
+    })
+
+    next.addEventListener('click', function(){
+        
+        counter++;
+        if(counter < 4){
+            figure[counter-1].classList.remove('show');
+            figure[counter].classList.add('show');
+        } else {
+            figure[counter-1].classList.remove('show');
+            counter = 0;
+            figure[counter].classList.add('show');
+        }
+
+        console.log("next",counter);
+    })
+    
+})()
---
+++ js/quatation.js
@@ -0,0 +1,44 @@
+(function(){
+    var figure = document.querySelectorAll(".slide-show figure");
+    var prev = document.querySelector(".slide-show .prev")
+    var next = document.querySelector(".slide-show .next")
+    
+        figure[0].classList.add('show');
+        var counter = 0;
+        
+        console.log(figure);
+        
+        prev.addEventListener('click', function(){
+            
+                if(counter == 0){
+                    counter = 1;
+                    figure[counter-1].classList.remove('show');
+                    figure[counter].classList.add('show');
+                } else if (counter > 0){
+                    figure[counter].classList.remove('show');
+                    counter--;
+                    figure[counter].classList.add('show');
+                }
+            
+            console.log("prev",counter);
+        })
+    
+        next.addEventListener('click', function(){
+            
+            counter++;
+            if(counter < 2){
+                figure[counter-1].classList.remove('show');
+                figure[counter].classList.add('show');
+            } else {
+                figure[counter-1].classList.remove('show');
+                counter = 0;
+                figure[counter].classList.add('show');
+            }
+    
+            console.log("next",counter);
+        })
+        
+    })()
+    
+    
+    
